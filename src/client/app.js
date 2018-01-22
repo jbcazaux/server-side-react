58849ecc43968e4ea51a4cf41c0b651226b95fca
jbcazaux <Jean-Baptiste.Cazaux@ext.saint-gobain.com>
2018-01-22 16:01:32 +0100

    simple routing on client and server sides

--- src/client/app.js
+++ src/client/app.js
@@ -1,6 +1,25 @@
 import React from 'react';
+import {Link, Route} from 'react-router-dom';
 
 class App extends React.Component {
+    render() {
+        return <div>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/counter">Counter</Link></li>
+                <li><Link to="/about">About</Link></li>
+            </ul>
+
+            <hr/>
+
+            <Route exact path="/" component={Home}/>
+            <Route path="/counter" component={Counter}/>
+            <Route path="/about" component={About}/>
+        </div>;
+    }
+}
+
+class Counter extends React.Component {
 
     constructor(props) {
         super(props);
@@ -19,5 +38,19 @@ class App extends React.Component {
         </div>;
     }
 }
+
+const Home = () => (
+    <div>
+        <h2>Home</h2>
+        Welcome !
+    </div>
+);
+
+const About = () => (
+    <div>
+        <h2>About</h2>
+        About this application...
+    </div>
+);
 
 export default App;
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import React from 'react';
 import {renderToString} from 'react-dom/server';
+import {StaticRouter} from 'react-router-dom';
 import App from './client/app';
 import Html from './client/html';
 
@@ -9,16 +10,27 @@ const server = express();
 
 server.use('/public', express.static('dist'));
 
-server.get('/', (req, res) => {
-    const body = renderToString(<App/>);
+server.get('*', (req, res) => {
+    const context = {};
+
+    const appWithRouter = (
+        <StaticRouter location={req.url} context={context}>
+            <App/>
+        </StaticRouter>
+    );
+
+    if (context.url) {
+        res.redirect(context.url);
+        return;
+    }
+
+    const body = renderToString(appWithRouter);
     const title = 'Server Side React';
 
-    res.send(
-        Html({
-            body,
-            title
-        })
-    );
+    res.status(200).send(Html({
+        body,
+        title
+    }));
 });
 
 server.listen(port);
