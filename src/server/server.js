0a6837ca3b6a5e71820b2616b363677776aa219d
jb cazaux <jbcazaux@gmail.com>
2018-02-05 22:14:18 +0100

    server side redirection

--- src/server/server.js
+++ src/server/server.js
@@ -58,7 +58,11 @@ server.get('/users', (req, res) => {
 server.get('*', (req, res) => {
     const context = {};
     const app = renderWithReduxState({counter: 1}, req.url, context);
-
+    if (context.url) {
+        console.log('will redirect to ', context.url);
+        res.redirect(context.url);
+        return;
+    }
     res.status(200).send(app);
 });
 
